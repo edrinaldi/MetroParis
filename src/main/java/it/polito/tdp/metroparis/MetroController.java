da98b005954055820bca3e046af6174ac01d39f5
edoardorinaldi <edoardorinaldi@mbp-di-edoardo.lan>
2022-05-02 07:36:18 +0200

    esercizio impostato.

--- src/main/java/it/polito/tdp/metroparis/MetroController.java
+++ src/main/java/it/polito/tdp/metroparis/MetroController.java
@@ -1,5 +1,14 @@
 package it.polito.tdp.metroparis;
 
+import it.polito.tdp.metroparis.model.Model;
+
 public class MetroController {
+	Model model;
+
+	public void setModel(Model model) {
+		// TODO Auto-generated method stub
+		this.model = model;
+		
+	}
 	
 }
---
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -0,0 +1,5 @@
+package it.polito.tdp.metroparis.model;
+
+public class Model {
+
+}
