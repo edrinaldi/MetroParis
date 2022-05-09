bcf30073d6d67aa31b5ac5d97966098587d6c3d9
edoardorinaldi <edoardorinaldi@localhost>
2022-05-09 10:46:22 +0200

    progetto a fine lez 09/05.

--- src/main/java/it/polito/tdp/metroparis/MetroController.java
+++ src/main/java/it/polito/tdp/metroparis/MetroController.java
@@ -1,14 +1,84 @@
+/**
+ * Sample Skeleton for 'Metro.fxml' Controller Class
+ */
+
 package it.polito.tdp.metroparis;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.metroparis.model.Fermata;
 import it.polito.tdp.metroparis.model.Model;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class MetroController {
+	
 	Model model;
 
-	public void setModel(Model model) {
-		// TODO Auto-generated method stub
-		this.model = model;
-		
-	}
-	
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="boxArrivo"
+    private ComboBox<Fermata> boxArrivo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="boxPartenza"
+    private ComboBox<Fermata> boxPartenza; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colFermata"
+    private TableColumn<Fermata, String> colFermata; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tablePercorso"
+    private TableView<Fermata> tablePercorso; // Value injected by FXMLLoader
+    
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML
+    void doRicercaPercorso(ActionEvent event) {
+    	Fermata partenza = this.boxPartenza.getValue();
+    	Fermata arrivo = this.boxArrivo.getValue();
+    	if (partenza != null && arrivo != null && !partenza.equals(arrivo)) {
+        	List<Fermata> percorso = this.model.calcolaPercorso(partenza, arrivo);
+        	this.tablePercorso.setItems(FXCollections.observableArrayList(percorso));
+        	this.txtResult.setText("Percorso trovato con " + percorso.size() + " stazioni.");
+    	}
+    	else {
+    		this.txtResult.setText("Devi selezionare 2 stazioni, diverse tra loro\n");
+    	}
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert boxArrivo != null : "fx:id=\"boxArrivo\" was not injected: check your FXML file 'Metro.fxml'.";
+        assert boxPartenza != null : "fx:id=\"boxPartenza\" was not injected: check your FXML file 'Metro.fxml'.";
+        assert colFermata != null : "fx:id=\"colFermata\" was not injected: check your FXML file 'Metro.fxml'.";
+        assert tablePercorso != null : "fx:id=\"tablePercorso\" was not injected: check your FXML file 'Metro.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Metro.fxml'.";
+
+        this.colFermata.setCellValueFactory(new PropertyValueFactory<Fermata, String>("nome"));
+    }
+    
+    public void setModel(Model model) {
+    	this.model = model;
+    	
+    	for (Fermata f : this.model.getFermate()) {
+    		this.boxPartenza.getItems().add(f);
+    	}
+    	
+    	for (Fermata f : this.model.getFermate()) {
+    		this.boxArrivo.getItems().add(f);
+    	}
+    }
+
 }
--- src/main/java/it/polito/tdp/metroparis/model/Model.java
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,21 +18,33 @@ import it.polito.tdp.metroparis.db.MetroDAO;
 public class Model {
 
 	private Graph<Fermata, DefaultEdge> grafo;
+	MetroDAO dao;
+	private List<Fermata> fermate;
+	Map<Integer, Fermata> fermateIdMap;
+	
+	public List<Fermata> getFermate() {
+		if (this.fermate == null) {
+			// e' la prima volta che chiamo il metodo
+			this.dao = new MetroDAO();
+			this.fermate = this.dao.getAllFermate();	// query impostata in ord alfab
+			
+			this.fermateIdMap = new HashMap<Integer, Fermata>();
+			for (Fermata f : this.fermate) {
+				fermateIdMap.put(f.getIdFermata(), f);
+			}
+		}
+		return this.fermate;
+	}
 	
 	public void creaGrafo() {
 		// istanziamo la struttura dati (il grafo stesso)
 		this.grafo = new SimpleDirectedGraph<Fermata, DefaultEdge>(DefaultEdge.class);
 		
-		MetroDAO dao = new MetroDAO();
-		List<Fermata> fermate = dao.getAllFermate();
-		Map<Integer, Fermata> fermateIdMap = new HashMap<Integer, Fermata>();
+		// inseriamo tutti i vertici
+		Graphs.addAllVertices(this.grafo, this.getFermate());	// meglio di this.fermate
+																// (progr difensiva)
 		
-		for (Fermata f : fermate) {
-			fermateIdMap.put(f.getIdFermata(), f);
-		}
-		
-		Graphs.addAllVertices(this.grafo, fermate);
-		
+		// inseriamo tutti gli archi
 		// METODO 1: itero su ogni coppia di vertici (NON SEMPRE e' il piu' lento!
 		//										      es. grafi piccoli)
 		/*
@@ -89,6 +102,7 @@ public class Model {
 
 		// METODO 3: faccio un'unica query che mi restituisca le coppie di fermate connesse
 		// variante preferita: 3c (identity map)
+		this.dao = new MetroDAO();
 		List<CoppiaId> fermateDaCollegare = dao.getAllFermateConnesse();
 		for (CoppiaId c : fermateDaCollegare) {
 			Fermata partenza = fermateIdMap.get(c.getIdPartenza());
@@ -96,20 +110,49 @@ public class Model {
 			this.grafo.addEdge(partenza, arrivo);
 		}
 
-		
-		// System.out.println(this.grafo);
-		System.out.println("Vertici: " + this.grafo.vertexSet().size());
-		System.out.println("Archi: " + this.grafo.edgeSet().size());
+//		System.out.println(this.grafo);
+//		System.out.println("Vertici: " + this.grafo.vertexSet().size());
+//		System.out.println("Archi: " + this.grafo.edgeSet().size());
 
-		this.visitaGrafo(fermate.get(0));
+//		this.visitaGrafo(fermate.get(0));
 	}
 
-	public void visitaGrafo(Fermata partenza) {
+	public List<Fermata> calcolaPercorso(Fermata partenza, Fermata arrivo) {
+		this.creaGrafo();
+		Map<Fermata, Fermata> alberoInverso = this.visitaGrafo(partenza);
+		Fermata corrente = arrivo;
+		List<Fermata> percorso = new ArrayList<>();
+		while (corrente != null) {
+			percorso.add(0, corrente);
+			corrente = alberoInverso.get(corrente);
+		}
+		return percorso;
+	}
+	
+	public Map<Fermata, Fermata> visitaGrafo(Fermata partenza) {
 		GraphIterator<Fermata, DefaultEdge> visita = 
-				new DepthFirstIterator<Fermata, DefaultEdge>(this.grafo, partenza);
+				new BreadthFirstIterator<Fermata, DefaultEdge>(this.grafo, partenza);
+		
+		Map<Fermata, Fermata> alberoInverso = new HashMap<>();
+		// aggiungo la radice, che non ha vertice padre
+		alberoInverso.put(partenza, null);
+		
+		visita.addTraversalListener(new RegistraAlberoDiVisita(alberoInverso, this.grafo));
 		while(visita.hasNext()) {
 			Fermata f = visita.next();
-			System.out.println(f);
+//			System.out.println(f + "\n");
 		}
+		
+		return alberoInverso;
+		
+		// ho l'albero inverso, posso ricostruire il percorso
+		// TODO
+//		List<Fermata> percorso = new ArrayList<>();
+//		fermata = arrivo;
+//		while(fermata != null) {
+//			fermata = alberoInverso.get(fermata);
+//		}
 	}
+	
+	
 }
